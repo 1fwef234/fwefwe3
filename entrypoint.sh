db4a0fa69bb13d561c427a45184d91f13ee10530
1fwef234 <142979418+1fwef234@users.noreply.github.com>
2023-08-23 09:24:21 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 UUID='89931024-fddc-4ba3-b288-aa41fce9bb34'
-VMESS_WSPATH='/aa41fce9bb34-vmess'
-VLESS_WSPATH='/aa41fce9bb34-vless'
-TROJAN_WSPATH='/aa41fce9bb34-trojan'
-SS_WSPATH='/aa41fce9bb34-shadowsocks'
+VMESS_WSPATH='/aa41fce9bb34vmess'
+VLESS_WSPATH='/aa41fce9bb34vless'
+TROJAN_WSPATH='/aa41fce9bb34trojan'
+SS_WSPATH='/aa41fce9bb34shadowsocks'
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
